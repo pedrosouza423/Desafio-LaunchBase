362b4a73c8fa037a1747559beb02026dd4c3a1f7
pedrosouza423 <pedro.souza4237@gmail.com>
2020-12-22 19:26:08 -0300

    Iniciando o Back-end, utilizando o node

---
+++ server.js
@@ -0,0 +1,31 @@
+//Importando express e nunjucks
+const express = require('express')
+const nunjucks = require('nunjucks')
+
+//Dando o servidor a função de um express
+const server = express()
+
+//Parte estática do site
+server.use(express.static("public"))
+
+//Setando o view engine do servidor
+server.set("view engine", "html")
+
+//Configurando nunjucks
+nunjucks.configure("views", {
+    express:server
+})
+
+//Rendenizando a página
+server.get("/", function(req, res){
+    return res.render('sobre')
+})
+
+server.get("/conteudo", function(req, res){
+    return res.render('conteudo')
+})
+
+//Rodando o servidor
+server.listen(5000, function(){
+    console.log('server is running')
+})
